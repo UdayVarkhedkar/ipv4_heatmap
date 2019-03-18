ecc196f8429fc5e08fbacb0b58988575b838e6f9
UdayVarkhedkar <uvarkhedkar@gmail.com>
2019-03-17 22:19:01 -0400

    Update settings for static file collection

--- application/application/settings.py
+++ application/application/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
